4740660ec8449d1ebfe67fe666edb831f95be0f5
darth-kermit <tristan.oetker@gmail.com>
2021-05-02 00:17:00 -0400

    I improved the output of the script to include a timestamp and better readability. This satisfies criteria ii and v from the requirements.

--- test_client.sh
+++ test_client.sh
@@ -5,8 +5,10 @@ while true
 do
     echo "Time Now: `date +%H:%M:%S`"
     echo "Curling Orcish Translater"
-    curl 3.14.254.63:8080/version
-    echo "END"
+    echo "Time Now: `date +%H:%M:%S`" >> test_client_results/deployment-results.txt
+    curl 3.14.254.63:8080/version >> test_client_results/deployment-results.txt
+    echo "" >> test_client_results/deployment-results.txt 
+    echo "------------------------------------------------" >> test_client_results/deployment-results.txt 
     wait_period=$(($wait_period+1))
     if [ $wait_period -gt 300 ];then
        echo "The script successfully ran for 5 minutes, exiting now.."
