cb90a228c5ae2d39461efc0921001ff59026c01a
darth-kermit <tristan.oetker@gmail.com>
2021-05-02 00:08:44 -0400

    I got the api up and running on ECS. I updated the script to curl the endpoint and check the version, which is working as expected.

--- test_client.sh
+++ test_client.sh
@@ -5,7 +5,7 @@ while true
 do
     echo "Time Now: `date +%H:%M:%S`"
     echo "Curling Orcish Translater"
-    curl -I -X GET https://worldofwarcraft.com/en-us/
+    curl 3.14.254.63:8080/version
     echo "END"
     wait_period=$(($wait_period+1))
     if [ $wait_period -gt 300 ];then
