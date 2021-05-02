6fd73ad8d290db5009eedb995c21b8378fe8c4b1
darth-kermit <tristan.oetker@gmail.com>
2021-05-02 01:47:51 -0400

    Adding loadbalancer endpoint for blue/green deployment endpoint

--- test_client.sh
+++ test_client.sh
@@ -6,7 +6,7 @@ do
     echo "Time Now: `date +%H:%M:%S`"
     echo "Curling Orcish Translater"
     echo "Time Now: `date +%H:%M:%S`" >> test_client_results/deployment-results.txt
-    curl 3.14.254.63:8080/version >> test_client_results/deployment-results.txt
+    curl http://test-endpoint-492633064.us-east-2.elb.amazonaws.com:8080/version >> test_client_results/deployment-results.txt
     echo "" >> test_client_results/deployment-results.txt 
     echo "------------------------------------------------" >> test_client_results/deployment-results.txt 
     wait_period=$(($wait_period+1))
